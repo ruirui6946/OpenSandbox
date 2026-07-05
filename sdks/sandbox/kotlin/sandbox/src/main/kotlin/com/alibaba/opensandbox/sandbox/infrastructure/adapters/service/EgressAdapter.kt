f9e76ab58a20030701e25665f417c1c17b613916
高然 <pangjiping.pjp@alibaba-inc.com>
2026-07-05 14:13:20 +0800

    style(kotlin): fix spotless formatting in EgressAdapter
    Sort imports alphabetically and reformat when-branch bodies.

--- sdks/sandbox/kotlin/sandbox/src/main/kotlin/com/alibaba/opensandbox/sandbox/infrastructure/adapters/service/EgressAdapter.kt
+++ sdks/sandbox/kotlin/sandbox/src/main/kotlin/com/alibaba/opensandbox/sandbox/infrastructure/adapters/service/EgressAdapter.kt
@@ -32,11 +32,11 @@ import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialListRes
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialMatch
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialMetadata
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialMutationSet
+import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialSource
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialVaultCreateRequest
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialVaultPatchRequest
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialVaultState
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CustomHeaderEntry
-import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.CredentialSource
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.HTTPCredentialSource
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.InlineCredentialSource
 import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.NetworkPolicy
@@ -317,18 +317,20 @@ internal class EgressAdapter(
 
     private fun CredentialSource.toJsonObject(): JsonObject =
         when (this) {
-            is InlineCredentialSource -> buildJsonObject {
-                put("type", JsonPrimitive(type))
-                put("value", JsonPrimitive(value))
-            }
-            is HTTPCredentialSource -> buildJsonObject {
-                put("type", JsonPrimitive(type))
-                put("url", JsonPrimitive(url))
-                put("method", JsonPrimitive(method))
-                headers?.let { h ->
-                    put("headers", buildJsonObject { h.forEach { (k, v) -> put(k, JsonPrimitive(v)) } })
+            is InlineCredentialSource ->
+                buildJsonObject {
+                    put("type", JsonPrimitive(type))
+                    put("value", JsonPrimitive(value))
                 }
-            }
+            is HTTPCredentialSource ->
+                buildJsonObject {
+                    put("type", JsonPrimitive(type))
+                    put("url", JsonPrimitive(url))
+                    put("method", JsonPrimitive(method))
+                    headers?.let { h ->
+                        put("headers", buildJsonObject { h.forEach { (k, v) -> put(k, JsonPrimitive(v)) } })
+                    }
+                }
         }
 
     private fun List<CredentialBinding>.toBindingJsonArray(): JsonArray = JsonArray(map { it.toJsonObject() })
